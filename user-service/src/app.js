09cbfc782bae563c1e834e7258104c62eff9c5f0
PabloAndresVillegas1 <pabloandresvillegas1@gmail.com>
2025-03-01 16:28:26 -0500

    Commit inicial

---
+++ user-service/src/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+app.use('/api/users', userRoutes);
+
+module.exports = app;
+
+
+
+/*const express = require('express');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+app.use(express.json());
+
+// Conectar a la base de datos
+connectDB();
+
+// Definir rutas
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando en el puerto ${PORT}`);
+});
+
+*/
